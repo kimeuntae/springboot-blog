ba6b3947e40a18cf43b12782a3fdbe9a35df1468
kimeuntae <rladmsxo6533@gmail.com>
2023-01-15 13:00:48 +0900

    no message

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean //IOC 가 된다.
 	public BCryptPasswordEncoder encodePWD() {
 		return new BCryptPasswordEncoder();
-	}
+	} 
 	
 	// 시큐리티가 대신 로그인해주는데 password를 가로채기를 하는데
 	// 해당 password가 뭘로 해쉬가 되어 회원가입이 되었는지 알아야
